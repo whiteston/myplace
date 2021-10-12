116b5021e03252962c8bf3a8d8b4f5a2bd757fa3
whiteston <dmsals765@naver.com>
2021-10-12 23:02:34 +0900

    not yet

---
+++ Baekjoon/BinarySearch/m.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n, m = list(map(int, input().split()))
+
+button = []
+for _ in range(m):
+    button.append(list(map(int, input().split())))
+
+def search(button, start, end):
+    for i in button:
+check[0] = button[]
+#start = 
+print(start)
