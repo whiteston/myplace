56e22f14887e2c141ff415e4c2c58fc9c1d752b7
whiteston <dmsals765@naver.com>
2021-10-13 21:03:22 +0900

    modified

--- Baekjoon/BinarySearch/m.py
+++ Baekjoon/BinarySearch/m.py
@@ -7,8 +7,19 @@ button = []
 for _ in range(m):
     button.append(list(map(int, input().split())))
 
-def search(button, start, end):
-    for i in button:
-check[0] = button[]
-#start = 
-print(start)
+def search(start, left, right):
+    count = 1
+    while count < n:
+        if left >= right:
+            left = left - right
+            count += 1
+        else:
+            left = left + start
+            count += 1
+    return left
+
+array = []
+for i in button:
+    array.append(search(i[0], i[0], i[1]))
+
+print(min(array))
